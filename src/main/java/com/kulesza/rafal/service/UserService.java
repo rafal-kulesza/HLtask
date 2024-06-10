8fdc8e552dcf28fc1485ba5619d060d1a55819d7
rkulesza <38611047+rafal-kulesza@users.noreply.github.com>
2024-06-10 22:11:48 +0200

    Injected repository dependency

--- src/main/java/com/kulesza/rafal/service/UserService.java
+++ src/main/java/com/kulesza/rafal/service/UserService.java
@@ -1,6 +1,12 @@
 package com.kulesza.rafal.service;
+import com.kulesza.rafal.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
