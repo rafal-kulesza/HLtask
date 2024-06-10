229d73381c1224aee414ce7c2bbea3f5c2f28567
rkulesza <38611047+rafal-kulesza@users.noreply.github.com>
2024-06-10 22:15:17 +0200

    Injected service dependency

--- src/main/java/com/kulesza/rafal/controller/UserController.java
+++ src/main/java/com/kulesza/rafal/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.kulesza.rafal.controller;
+
+import com.kulesza.rafal.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
