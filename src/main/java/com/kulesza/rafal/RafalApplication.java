5b707e0d2322fd35aa804f295b05b7d627e002d9
rkulesza <38611047+rafal-kulesza@users.noreply.github.com>
2024-06-10 21:49:06 +0200

    Initial commit

---
+++ src/main/java/com/kulesza/rafal/RafalApplication.java
@@ -0,0 +1,11 @@
+package com.kulesza.rafal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RafalApplication.class, args);
+	}
+}
