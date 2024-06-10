578282569b89de34121fcd88da39d34f7faef486
rkulesza <38611047+rafal-kulesza@users.noreply.github.com>
2024-06-10 22:16:32 +0200

    Uploaded default integration test

---
+++ src/test/java/com/kulesza/rafal/RafalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kulesza.rafal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
